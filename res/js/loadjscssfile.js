4b1ab976ebc2df86ede45f1aa2229b358149c0f8
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2018-08-28 09:38:18 -0400

    Added welcome and main pages

---
+++ res/js/loadjscssfile.js
@@ -0,0 +1,9 @@
+function loadjscssfile(filename, filetype) {
+    if (filetype=="css") { 
+        var fileref=document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", filename)
+    } if (typeof fileref!="undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+}
